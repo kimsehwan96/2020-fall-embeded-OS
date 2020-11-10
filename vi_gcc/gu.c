549006ea97989bf1011331a14f2b0896a8122b6c
Kimsehwan96 <sktpghks138@gmail.com>
2020-11-10 17:07:32 +0900

    add one more conditionc check

--- vi_gcc/gu.c
+++ vi_gcc/gu.c
@@ -7,7 +7,7 @@
            scanf("%d",&dan);
            if (dan == 0) {
                return 0;
-          } else if(dan>9){
+          } else if(dan>9 || dan < 0){
               printf("Wrong input...(0:quit, 2~9: dan    )\n");
               continue;
           }
